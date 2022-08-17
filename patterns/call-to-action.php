35f3c60ca0d848857bc24d14fedc975f0f389722
Carolina Nymark <hi@themesbycarolina.com>
2022-08-17 12:10:20 +0200

    Update call-to-action.php

--- patterns/call-to-action.php
+++ patterns/call-to-action.php
@@ -9,17 +9,16 @@
 ?>
 <!-- wp:columns {"align":"wide"} -->
 <div class="wp-block-columns alignwide">
-	<!-- wp:column {"width":"40%"} -->
-	<div class="wp-block-column" style="flex-basis:40%">
-		<!-- wp:paragraph {"fontSize":"x-large"} -->
-		<p class="has-x-large-font-size">
-		<?php esc_html_e( 'Get daily reflections in your inbox.', 'twentytwentythree' ); ?>
-		</p><!-- /wp:paragraph -->
+	<!-- wp:column -->
+	<div class="wp-block-column">
+		<!-- wp:paragraph {"style":{"typography":{"lineHeight":"1.2"}},"fontSize":"x-large"} -->
+		<p class="has-x-large-font-size" style="line-height:1.2">Got any book recommendations?</p>
+		<!-- /wp:paragraph -->
 
 		<!-- wp:buttons -->
 		<div class="wp-block-buttons"><!-- wp:button {"fontSize":"small"} -->
 		<div class="wp-block-button has-custom-font-size has-small-font-size"><a class="wp-block-button__link wp-element-button">
-			<?php esc_html_e( 'Join our mailing list', 'twentytwentythree' ); ?>
+			<?php esc_html_e( 'Get In Touch', 'twentytwentythree' ); ?>
 		</a></div>
 		<!-- /wp:button --></div>
 		<!-- /wp:buttons --></div>
